7509211ae0e4c51226e83953fe0c03da8d2eec78
Steven Young <stevenyoung131@gmail.com>
2017-07-31 21:07:55 -0400

    Fix formatting to conform to PEP8 style

--- project.py
+++ project.py
@@ -46,29 +46,29 @@ def fbconnect():
     access_token = request.data
     print "access token received %s " % access_token
 
-
     app_id = json.loads(open('fb_client_secrets.json', 'r').read())[
         'web']['app_id']
     app_secret = json.loads(
         open('fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type='
+    url += 'fb_exchange_token&client_id'
+    url += '=%s&client_secret=%s&fb_exchange_token=%s' % (
         app_id, app_secret, access_token)
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
 
-
     # Use token to get user info from API
     userinfo_url = "https://graph.facebook.com/v2.8/me"
-    ''' 
-        Due to the formatting for the result from the server token exchange we have to 
-        split the token first on commas and select the first index which gives us the key : value 
-        for the server access token then we split it on colons to pull out the actual token value
-        and replace the remaining quotes with nothing so that it can be used directly in the graph
-        api calls
-    '''
+    # Due to the formatting for the result from the server token exchange we
+    # have to split the token first on commas and select the first index
+    # which gives us the key : value for the server access token then we
+    # split it on colons to pull out the actual token value and replace the
+    # remaining quotes with nothing so that it can be used directly in the
+    # graph api calls
     token = result.split(',')[0].split(':')[1].replace('"', '')
 
-    url = 'https://graph.facebook.com/v2.8/me?access_token=%s&fields=name,id,email' % token
+    url = 'https://graph.facebook.com/v2.8/me?access_token'
+    url += '=%s&fields=name,id,email' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     # print "url sent for API access:%s"% url
@@ -83,7 +83,8 @@ def fbconnect():
     login_session['access_token'] = token
 
     # Get user picture
-    url = 'https://graph.facebook.com/v2.8/me/picture?access_token=%s&redirect=0&height=200&width=200' % token
+    url = 'https://graph.facebook.com/v2.8/me/picture?access_token'
+    url += '=%s&redirect=0&height=200&width=200' % token
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -103,8 +104,8 @@ def fbconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
-
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;'
+    output += '-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("Now logged in as %s" % login_session['username'])
     return output
 
@@ -114,7 +115,8 @@ def fbdisconnect():
     facebook_id = login_session['facebook_id']
     # The access token must me included to successfully logout
     access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (facebook_id,access_token)
+    url = 'https://graph.facebook.com/%s/permissions?access_token=%s'\
+        % (facebook_id, access_token)
     h = httplib2.Http()
     result = h.request(url, 'DELETE')[1]
     return "you have been logged out"
@@ -172,8 +174,8 @@ def gconnect():
     stored_access_token = login_session.get('access_token')
     stored_gplus_id = login_session.get('gplus_id')
     if credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already connected.'),
-                                 200)
+        response = make_response(json.dumps('Current user is\
+            already connected.'), 200)
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -206,7 +208,8 @@ def gconnect():
     output += '!</h1>'
     output += '<img src="'
     output += login_session['picture']
-    output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+    output += ' " style = "width: 300px; height: 300px;border-radius: 150px'
+    output += ';-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
     flash("You are now logged in as %s." % login_session['username'])
     print "done!"
     return output
@@ -256,7 +259,8 @@ def gdisconnect():
         response.headers['Content-Type'] = 'application/json'
         return response
     else:
-        response = make_response(json.dumps('Failed to revoke token for given user.', 400))
+        response = make_response(json.dumps('Failed to revoke token for\
+                                 given user.', 400))
         response.headers['Content-Type'] = 'application/json'
         return response
 
@@ -354,7 +358,8 @@ def editItem(item):
             own item in order to edit.")
         return render_template('viewitem.html',
                                item=item,
-                               categories=categories)
+                               categories=categories,
+                               login_session=login_session)
     if request.method == 'POST':
         if request.form['name']:
             item.name = request.form['name']
@@ -382,7 +387,8 @@ def deleteItem(item):
             own item in order to delete.")
         return render_template('viewitem.html',
                                item=item,
-                               categories=categories)
+                               categories=categories,
+                               login_session=login_session)
     if request.method == 'POST':
         session.delete(item)
         session.commit()
